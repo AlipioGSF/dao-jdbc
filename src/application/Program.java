aaf324ddd974aa94e2ffa58667c4edad6a8bf3f2
AlipioGSF <alipiotec@gmail.com>
2023-07-11 18:16:02 -0300

    adicionado Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,20 @@
 package application;
 
+import java.util.Date;
+
+import model.entitites.Department;
+import model.entitites.Seller;
+
 public class Program {
 	public static void main(String[] args) {
-	
+		
+		Department obj = new Department(1, "Books");
+		
+		
+		Seller seller = new Seller(1,"Tiko","Tiko@email.com", new Date(),2000.00,obj);
+		
+		System.out.println(seller.toString());
+		
 	}
 
 }
