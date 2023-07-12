b8b58c3264675c10ccf9ae462b9462f4a0490335
AlipioGSF <alipiotec@gmail.com>
2023-07-11 22:58:49 -0300

    adicionado findById do SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,18 @@
 package application;
 
-import java.util.Date;
-
-import model.entitites.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entitites.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(62);
 		
-		Seller seller = new Seller(1,"Tiko","Tiko@email.com", new Date(),2000.00,obj);
+		System.out.println(seller);
 		
-		System.out.println(seller.toString());
 		
 	}
 
